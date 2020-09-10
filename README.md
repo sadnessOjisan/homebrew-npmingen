dcba8ab7d88d46f1a38894ddff66edd8e010088a
sadnessOjisan <sadness.ojisan@gmail.com>
2020-09-10 21:57:51 +0900

    edit sha

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # homebrew-npmingen
 npmingenのFormula用レポジトリ
+
+## dev
+```
+# refresh
+rm /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/npmingen.rb 
+
+# create sha
+openssl sha1 path/to/file
+```
--- npmingen.rb
+++ npmingen.rb
@@ -6,7 +6,7 @@ class Npmingen < Formula
     homepage ""
     url "https://github.com/sadnessOjisan/npmingen/releases/latest/download/npmingen.zip"
     version "v0.0.8"
-    sha256 "3d826c1429794d51bdaadad4e3d1820072fccb82"
+    sha256 "d33a9e0e863cf4dec92ec41eb574ee2c47d88b6d56d4d88d6be33a7caed8eed9"
     license ""
   
     # depends_on "cmake" => :build
