6f5cbdc64e7aa576601533930e4f39529ce3ff63
sadnessOjisan <sadness.ojisan@gmail.com>
2020-09-10 21:43:19 +0900

    verup

--- npmingen.rb
+++ npmingen.rb
@@ -5,7 +5,7 @@ class Npmingen < Formula
     desc ""
     homepage ""
     url "https://github.com/sadnessOjisan/npmingen/releases/latest/download/npmingen.zip"
-    version "v0.0.4"
+    version "v0.0.5"
     sha256 ""
     license ""
   
